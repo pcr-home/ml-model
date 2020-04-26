51cc5dd927a42725ee3615dce2141c32fcb90e53
stephanieezhang <stephaniezhang902@gmail.com>
2020-04-25 22:05:47 -0700

    LSTM network
    finished data pre-processing and trained first iteration of LSTM NN!

---
+++ test.py
@@ -0,0 +1,95 @@
+import numpy as np
+import glob
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+np.random.seed(7)
+X_train = []
+Y_train = []
+filenames_txt = glob.glob('*.txt') 
+filenames_fna = glob.glob('*.fna')
+
+print(filenames_txt)
+
+def process_file(filename): 
+  # input: str filename 
+  
+  # Sets y_i to 1 if it starts with MN and 0 otherwise. 
+  y_i = 0
+  if filename[:2] == 'MN':  
+    y_i = 1
+
+  file = open(filename,encoding="latin-1") # opens the file
+  string = file.read() # this takes the file and reads it 
+  string_split = string.split('\n') # separates it based on the newline 
+  # we will only be concerned from string_split[1] onwards since the 
+  # first line has no value to us. I see :)
+  string_split = string_split[1:]
+  
+  # join all of them together to form one long string 
+  combined_sequence = ''.join(string_split) 
+  
+  # empty list 
+  X_i = []
+  for i in range(len(combined_sequence)): 
+    if combined_sequence[i]=='A':
+      X_i.append(1)
+    elif combined_sequence[i]=='C':
+      X_i.append(2)
+    elif combined_sequence[i]=='T':
+      X_i.append(3)
+    elif combined_sequence[i] == 'G':
+      X_i.append(4)
+       
+  return X_i, y_i
+
+# now we enumerate over the files 
+
+# create an empty list 
+list_Xtrain = [] 
+list_ytrain = []
+
+filenames = filenames_txt + filenames_fna
+
+for files in filenames: 
+  Xi, yi = process_file(files)
+  list_Xtrain.append(Xi)
+  list_ytrain.append(yi)
+
+X_train = np.array(list_Xtrain)
+y_train = np.array(list_ytrain)
+  
+print(X_train, y_train)
+
+list_Xtrain = [] 
+list_ytrain = []
+
+filenames = filenames_txt + filenames_fna
+
+for files in filenames: 
+  Xi, yi = process_file(files)
+  list_Xtrain.append(Xi)
+  list_ytrain.append(yi)
+
+X_train = np.array(list_Xtrain)
+y_train = np.array(list_ytrain)
+  
+print(X_train, y_train)
+  
+#from the tutorial:
+
+max_review_length = 30100
+X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+#X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+embedding_vecor_length = 5
+model = Sequential()
+model.add(Embedding(5, embedding_vecor_length, input_length=max_review_length)) #embeddings are meant to downsize input
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print(model.summary())
+
+model.fit(X_train, y_train, epochs=3, batch_size= 64)
